39ff5f7e18412297b24ac6d72155cb47a5f6515d
Marcial Ambriz <mackial@rebits.com.mx>
2019-10-05 03:02:09 -0500

    Segunda clase concluida

---
+++ clase2.js
@@ -0,0 +1,27 @@
+var edad = 23
+edad += 1 //incremento
+
+var peso = 88
+peso -= 2 //decremento
+
+var sandwich = 1
+peso += sandwich
+
+var hacerDeporte = 3
+peso -= hacerDeporte
+
+var precioVino = 200.3
+//Math.round redondea los valores segun su decimal, hacia arriba o abajo
+var total = Math.round(precioVino * 3)
+//Asi podemos usar el valor de PI
+var valorDePI = Math.PI
+//Con este modulo asignamos el numero de decimales para mostrar en lso valores
+var valorShort = valorDePI.toFixed(4)
+//transforma de un String a un valor numero de Float
+var valorNumericoPi = parseFloat(valorShort)
+
+var pizza = 8
+var personas = 2
+var CantidadPorcionesPorPersonas = pizza / 2
+
+
