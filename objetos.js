a840355e6b330a507ec8b50055d1a0d82e851953
Marcial Ambriz <mackial@rebits.com.mx>
2019-10-07 03:23:25 -0500

    Clase funcines con referencias o valores

--- objetos.js
+++ objetos.js
@@ -15,13 +15,23 @@ function imprimirNombreMayusculas(persona) {
     console.log(nombre)
 }
 
+//imprimirNombreMayusculas(marcial)
+//imprimirNombreMayusculas(marco)
+
 function imprimirNombreEdad(persona){
     var {nombre} = persona, {apellido} = persona, {edad} = persona
     console.log(`Hola, mi nombre es ${nombre} ${apellido} y tengo ${edad} años`)
 }
 
-imprimirNombreEdad(marcial)
-imprimirNombreEdad(marco)
+//imprimirNombreEdad(marcial)
+//imprimirNombreEdad(marco)
 
-//imprimirNombreMayusculas(marcial)
-//imprimirNombreMayusculas(marco)
+function cumple(persona) {
+    //Regresa un objeto nuevo, mismos parametros del objeto que recibe
+    return {
+        //Desglosa los parametros del objeto recibido
+        ...persona, 
+        //Se pueden modificar e incluso agregar nuevos parametros
+        edad: persona.edad + 1
+    }
+}
