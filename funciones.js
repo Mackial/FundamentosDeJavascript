36c686f9349b71072283337de8db57f4b3e3f1db
Marcial Ambriz <mackial@rebits.com.mx>
2019-10-07 02:54:56 -0500

    Clase funciones

---
+++ funciones.js
@@ -0,0 +1,14 @@
+var nombre = 'Sacha' , edad = 28
+
+function imprimirEdad(n, e){
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+
+function imprimirNombreMayusculas(n){
+    n = n.toUpperCase()
+    console.log(n)
+}
+
+imprimirNombreMayusculas(nombre)
