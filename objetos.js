1a93e144af2fa56a947a3ae0881b9415d73f46c4
Marcial Ambriz <mackial@rebits.com.mx>
2019-10-07 03:11:31 -0500

    Clase de funciones

---
+++ objetos.js
@@ -0,0 +1,19 @@
+var marcial = {
+    nombre: 'Marcial',
+    apellido: 'Ambriz',
+    edad: 23
+}
+
+var marco = {
+    nombre: 'Marco',
+    apellido: 'Loera',
+    edad: 22
+}
+
+function imprimirNombreMayusculas(persona) {
+    var nombre = persona.nombre.toUpperCase()
+    console.log(nombre)
+}
+
+//imprimirNombreMayusculas(marcial)
+//imprimirNombreMayusculas(marco)
