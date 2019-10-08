ce2568431a27654df6b3ff4d1f3d76327073884d
Marcial Ambriz <mackial@rebits.com.mx>
2019-10-08 05:13:10 -0500

    Clase Switch

---
+++ horoscopo.js
@@ -0,0 +1,37 @@
+var signo = prompt('¿Cual es tu signo Zodiacal?')
+
+switch (signo) {
+    case 'acuario':
+        console.log('No es una sorpresa que te sientas algo reacia a aceptar otra situación complicada.')
+        break
+    case 'cancer':
+    case 'cáncer':
+        console.log('Los gurús de la Nueva Era suelen decir que el universo sólo quiere lo mejor para nosotros, que existe para apoyarnos.')
+        break
+    case 'virgo':
+    case 'puto':
+        console.log('Los gurús de la Nueva Era suelen decir que el universo sólo quiere lo mejor para nosotros, que existe para apoyarnos.')
+        break
+    case 'libra':
+        console.log('Los gurús de la Nueva Era suelen decir que el universo sólo quiere lo mejor para nosotros, que existe para apoyarnos.')
+        break
+    default:
+        console.log('No se encontro tu horoscopo')
+        break
+}
+
+if (signo === 'acuario') {
+    
+}
+if (signo === 'cancer') {
+    
+
+}
+if (signo === 'virgo') {
+    
+
+}
+if (signo === 'libra') {
+    
+
+}
