0b9e1de2982463a55467c6e0fe61c827fe3c3ae4
Marcial Ambriz <mackial@rebits.com.mx>
2019-10-08 04:48:45 -0500

    Clase Ciclo While

--- ciclosfor.js
+++ ciclosfor.js
@@ -7,21 +7,27 @@ var marcial = {
 
 console.log(`Al inicio del año ${marcial.nombre} pesa ${marcial.peso}kg`)
 
-const INCREMENTO_PESO =0.2
+const INCREMENTO_PESO = 0.2
 const YEAR = 365
 
 const aumentarPeso = persona => persona.peso += INCREMENTO_PESO
 const perderPeso = persona => persona.peso -= INCREMENTO_PESO
 
+const gloton = () => Math.random() < 0.3
+const actividad = () => Math.random() < 0.4
 
-for (var i = 1; i <= YEAR; i++) {
-    var random = Math.random()
+const META = marcial.peso - 3
+var dias = 0
 
-    if (random < 0.25) {
+while (marcial.peso > META) {
+    if (gloton()) {
         aumentarPeso(marcial)
-    } else if (random < 0.5) {
+    }
+
+    if (actividad()) {
         perderPeso(marcial)
     }
+    dias += 1
 }
 
-console.log(`Al final del año ${marcial.nombre} pesa ${marcial.peso.toFixed(1)}kg`)
+console.log(`Pasaron ${dias} dias para que ${marcial.nombre} cumpliera la meta de pesar ${marcial.peso.toFixed(1)}kg`)
