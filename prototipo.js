a9b711b9aa85a8f4b231603c93b0cf4cf57bfca1
Marcial Ambriz <mackial@rebits.com.mx>
2019-10-15 16:20:18 -0500

    Clase funcion como parametro

--- prototipo.js
+++ prototipo.js
@@ -6,32 +6,42 @@ class Persona {
         this.altura = altura
     }
 
-    saludar() {
+    saludar(fn) {
         console.log(`hola, me llamo ${this.nombre} ${this.apellido}`)
     }
-
+    
     soyAlto() {
         return this.altura >= 1.8
     }
-
-
+    
+    
 }
 
 //Con estends hacemos referencia la herencia de otra funcion o clase
 class Desarrollador extends Persona {
-
+    
     constructor(nombre, apellido, edad, altura) {
-    //para llamar el constructor de la clase o funcion heredada usamos super
+        //para llamar el constructor de la clase o funcion heredada usamos super
         super(nombre, apellido, edad, altura)
     }
-
-    saludar() {
+    
+    saludar(fn) {
         console.log(`hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)    
+        if (fn) {
+            fn(this.nombre, this.apellido, true)
+        }
     }
 }
 
-//var sacha = new Persona('Sacha', 'Lifszyc', 27, 1.72)
-//var erika = new Persona('Erika', 'Luna', 24, 1.57)
-//var marcial = new Persona('Marcial', 'Ambriz', 23, 1.82)
+function responderSaludo(nombre, apellido, esDev) {
+    console.log(`Buen dia ${nombre} ${apellido}`)
+    if (esDev) {
+        console.log(`Ah, no sabia que eras desarrollador`)
+    }
+}
+
+var sacha = new Persona('Sacha', 'Lifszyc', 27, 1.72)
+var erika = new Persona('Erika', 'Luna', 24, 1.57)
+var marcial = new Desarrollador('Marcial', 'Ambriz', 23, 1.82)
 
 //sacha.saludar()
