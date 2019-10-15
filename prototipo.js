6253675d2122d668d68d5cdbb75a0d69e9166668
Marcial Ambriz <mackial@rebits.com.mx>
2019-10-15 15:59:20 -0500

    Clase herencia de clases

--- prototipo.js
+++ prototipo.js
@@ -1,39 +1,37 @@
-function heredaDe(prototipoHijo, prototipoPadre) {
-    var fn = function () {}
+class Persona {
+    constructor(nombre, apellido, edad, altura) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.edad = edad
+        this.altura = altura
+    }
 
-    fn.prototype = prototipoPadre.prototype
-    prototipoHijo.prototype = new fn
-    prototipoHijo.prototype.constructor = prototipoHijo
+    saludar() {
+        console.log(`hola, me llamo ${this.nombre} ${this.apellido}`)
+    }
+
+    soyAlto() {
+        return this.altura >= 1.8
+    }
+
+
 }
 
-function Persona(nombre, apellido, edad, altura) {
-    this.nombre = nombre
-    this.apellido = apellido
-    this.edad = edad
-    this.altura = altura
+//Con estends hacemos referencia la herencia de otra funcion o clase
+class Desarrollador extends Persona {
+
+    constructor(nombre, apellido, edad, altura) {
+    //para llamar el constructor de la clase o funcion heredada usamos super
+        super(nombre, apellido, edad, altura)
+    }
+
+    saludar() {
+        console.log(`hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)    
+    }
 }
 
-Persona.prototype.saludar = function() {
-    console.log(`hola, me llamo ${this.nombre} ${this.apellido}`)
-}
-Persona.prototype.esAlto = function() {
-    return this.altura >= 1.8
-}
-
-function Desarrollador(nombre, apellido) {
-    this.nombre
-    this.apellido
-}
-
-heredaDe(Desarrollador, Persona)
-
-Desarrollador.prototype.saludar = function() {
-    console.log(`hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)    
-}
-
-
-var sacha = new Persona('Sacha', 'Lifszyc', 27, 1.72)
-var erika = new Persona('Erika', 'Luna', 24, 1.57)
-var marcial = new Persona('Marcial', 'Ambriz', 23, 1.82)
+//var sacha = new Persona('Sacha', 'Lifszyc', 27, 1.72)
+//var erika = new Persona('Erika', 'Luna', 24, 1.57)
+//var marcial = new Persona('Marcial', 'Ambriz', 23, 1.82)
 
 //sacha.saludar()
