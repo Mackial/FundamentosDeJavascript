57905d42e465be751f1872021ac1c9cf33446af1
Marcial Ambriz <mackial@rebits.com.mx>
2019-10-15 15:47:54 -0500

    Clase herenca de prototipos

--- prototipo.js
+++ prototipo.js
@@ -1,3 +1,11 @@
+function heredaDe(prototipoHijo, prototipoPadre) {
+    var fn = function () {}
+
+    fn.prototype = prototipoPadre.prototype
+    prototipoHijo.prototype = new fn
+    prototipoHijo.prototype.constructor = prototipoHijo
+}
+
 function Persona(nombre, apellido, edad, altura) {
     this.nombre = nombre
     this.apellido = apellido
@@ -10,6 +18,17 @@ Persona.prototype.saludar = function() {
 }
 Persona.prototype.esAlto = function() {
     return this.altura >= 1.8
+}
+
+function Desarrollador(nombre, apellido) {
+    this.nombre
+    this.apellido
+}
+
+heredaDe(Desarrollador, Persona)
+
+Desarrollador.prototype.saludar = function() {
+    console.log(`hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)    
 }
 
 
