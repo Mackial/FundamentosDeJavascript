d6eb48960d475c58820dd2f7ee94424c54e7414b
Marcial Ambriz <mackial@rebits.com.mx>
2019-10-15 01:59:18 -0500

    Clase reducir array a un valor

--- arrays.js
+++ arrays.js
@@ -1,27 +1,32 @@
 var sacha = {
     nombre: 'Sacha',
     apellido: 'Lifszyc',
-    altura: 1.72
+    altura: 1.72,
+    cantidadDeLibros: 111
 }
 var alan = {
     nombre: 'Alan',
     apellido: 'Perez',
-    altura: 1.86
+    altura: 1.86,
+    cantidadDeLibros: 138
 }
 var martin = {
     nombre: 'Martin',
     apellido: 'Gomez',
-    altura: 1.85
+    altura: 1.85,
+    cantidadDeLibros: 132
 }
 var vicky = {
     nombre: 'Vicky',
     apellido: 'Zapata',
-    altura: 1.56
+    altura: 1.56,
+    cantidadDeLibros: 90
 }
 var paula = {
     nombre: 'Paula',
     apellido: 'Barros',
-    altura: 1.76
+    altura: 1.76,
+    cantidadDeLibros: 91
 }
 //Condicion para el filtro
 const esAlta = ({altura}) => altura > 1.8
@@ -56,4 +61,17 @@ const pasarAlturaACmsMejor = persona => ({
 var personasCms = personas.map(pasarAlturaACms)
 //console.log(personasAltas)
 
-console.log(personasCms)
+var acum = 0
+
+for (var i = 0; i < personas.length; i++) {
+    acum = acum + personas[i].cantidadDeLibros
+}
+
+const reducer = (acum, persona) => {
+    return acum + persona.cantidadDeLibros
+}
+
+//reduce sirve para reducir todos los valores de un atributo a uno en un total
+var totalDeLibros = personas.reduce(reducer, 0)
+
+console.log(`El total que tienen entre todos es de ${totalDeLibros} libros`)
